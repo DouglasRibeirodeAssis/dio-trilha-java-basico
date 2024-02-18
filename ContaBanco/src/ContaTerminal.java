971cffdbb73d6820ac0a8eeb4749f1bad4d392bc
DouglasRibeirodeAssis <douglasribeirodeassis2003@gmail.com>
2024-02-17 23:37:10 -0300

    Meu projeto conta banco

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -28,7 +28,6 @@ public class ContaTerminal {
         System.out.println("Por favor digite o seu saldo!");
         double saldo = sc.nextDouble();
         System.out.println();
-        
 
         System.out.println("Olá "+ nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +", conta " + numero + "e seu saldo " + saldo + " já está disponível para saque");
     }
